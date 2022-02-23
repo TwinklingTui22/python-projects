b2e403319982aae5eb6b636d2283da3d287a1a71
TUI <nazmun47@student.sust.edu>
2022-02-23 10:47:45 +0600

    Added Automated Birthday Wisher Project

---
+++ Automated_birthday_wisher/main.py
@@ -0,0 +1,38 @@
+import datetime as dt
+import pandas
+import  random
+import smtplib
+
+MY_EMAIL = "dummy@gmail.com"
+MY_PASSWORD = "12345678"
+
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"] )
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"],
+        msg=f"Subject:Happy Birthday!\n\n{contents}")
+
+
+
+
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+# HINT 2: Remember to call .starttls()
+# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
+# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
+
+
+
