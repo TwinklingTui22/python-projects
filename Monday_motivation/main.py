8088f97d905c8a6aedf8814de5fab278aa109ac0
TUI <nazmun47@student.sust.edu>
2022-02-23 10:18:01 +0600

    Added Monday Motivation Project

---
+++ Monday_motivation/main.py
@@ -0,0 +1,19 @@
+import smtplib
+import datetime as dt
+import random
+
+my_email = "dummy@gmail.com"
+password = "12345678"
+
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+        with  smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=my_email,
+            msg=f"Subject:Monday Motivation\n\n{quote}")
